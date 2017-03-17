f7ac8ad45565fcd96abd9b847a7cacb6355911f9
dinidininta <dini.dininta@gmail.com>
2017-03-17 07:43:57 +0700

    test form 7

--- anandhibot/views.py
+++ anandhibot/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from anandhi.forms import InputForm
+from anandhibot.forms import InputForm
 
 
 # Create your views here
