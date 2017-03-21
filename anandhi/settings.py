1def85f5f965e25f04cf554634a466697e70f656
dinidininta <dini.dininta@gmail.com>
2017-03-22 00:40:26 +0700

    coba line lagi

--- anandhi/settings.py
+++ anandhi/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'anandhibot'
+    'anandhibot',
+    'rest_framework'
 ]
 
 MIDDLEWARE_CLASSES = [
--- anandhibot/urls.py
+++ anandhibot/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
      # url(r'^$', views.HomePageView.as_view()),
-     url(r'^$', TemplateView.as_view(template_name = 'input.html')),
+     url(r'^input', TemplateView.as_view(template_name = 'input.html')),
    	 url(r'^output/', views.input, name = 'input'),
-   	 url(r'^callback/', views.callback, name='callback')
+   	 url(r'^$', views.callback, name='callback')
 ]
